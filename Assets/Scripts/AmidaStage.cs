11c7cd9f8abb4317f6c4f8a5d623bf37dc46b771
koob <kouchibooks@gmail.com>
2020-04-09 21:31:14 +0900

    set for UI

---
+++ Assets/Scripts/AmidaStage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerater : MonoBehaviour
+{
+	[SerializeField] GameObject verticalLine;
+	[SerializeField] GameObject horizontalLine;
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
