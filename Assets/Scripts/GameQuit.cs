65f0c6a478c848e6a887ce6ac2a74987065f030a
koob <kouchibooks@gmail.com>
2020-05-13 16:57:05 +0900

    implement GameQuitSystem

---
+++ Assets/Scripts/GameQuit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GameQuit : MonoBehaviour
+{
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+			EditorApplication.isPlaying = false;
+		#elif UNITY_STANDALONE
+			Application.Quit();
+		#endif
+	}
+}
