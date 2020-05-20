03e26ea05834e0d1fa84e13bdb970d40b17eec7a
koob <kouchibooks@gmail.com>
2020-05-20 17:18:13 +0900

    Fix Code

--- Assets/Scripts/HorizontalLine.cs
+++ Assets/Scripts/HorizontalLine.cs
@@ -47,7 +47,7 @@ public class HorizontalLine : MonoBehaviour
 		return OnObj.activeSelf;
 	}
 
-	private void OnTriggerEnter2D(Collider2D collision)
+	private void OnTriggerStay2D(Collider2D collision)
 	{
 		if (collision.gameObject.tag == "food")
 		{
