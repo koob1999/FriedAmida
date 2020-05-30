7b660e26093f033a3114da4109212e7f36d9ba35
koob <kouchibooks@gmail.com>
2020-05-30 23:28:10 +0900

    Implement EndingScore

---
+++ Assets/Scripts/EndingManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingManager : MonoBehaviour
+{
+	[SerializeField] Text noonScore;
+	[SerializeField] Text eveningScore;
+	[SerializeField] Text midnightScore;
+
+	void Start()
+	{
+		noonScore.text = "昼ステージ：" + ScoreData.NoonScore.ToString();
+		eveningScore.text = "夕方ステージ：" + ScoreData.EveningScore.ToString();
+		midnightScore.text = "夜ステージ：" + ScoreData.MidnightScore.ToString();
+	}
+}
---
+++ Assets/Scripts/ScoreData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class ScoreData 
+{
+	public static int NoonScore = 0;
+	public static int EveningScore = 0;
+	public static int MidnightScore = 0;
+
+	public static void ResetScore()
+	{
+		NoonScore = 0;
+		EveningScore = 0;
+		MidnightScore = 0;
+	}
+
+	public static void SaveScore(int score)
+	{
+		switch (SceneManager.GetActiveScene().name)
+		{
+			case "Noon":
+				NoonScore = score;
+				break;
+			case "Evening":
+				EveningScore = score;
+				break;
+			case "Midnight":
+				MidnightScore = score;
+				break;
+			default:
+				break;
+		}
+	}
+}
